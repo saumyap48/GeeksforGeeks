486d47fd4cc7753c98caecf1cafff1bc635c2156
Saumya Pandey <saumyap271@gmail.com>
2024-11-30 07:31:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+         int n=arr.size();
+         for(int i=0; i<n-1; i++){
+             int index=i;
+             for(int j=i+1; j<n; j++){
+                 if(arr[j]<arr[index])
+                 index=j;
+             }
+             swap(arr[i],arr[index]);
+         }
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution obj;
+        obj.selectionSort(a);
+
+        Array::print(a);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
