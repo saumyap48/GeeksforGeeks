9c997e4444ffc3752197b260d9a5e0c5ae2d44c7
Saumya Pandey <saumyap271@gmail.com>
2024-12-03 20:04:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.cpp
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.cpp
@@ -6,39 +6,40 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int minChar(string& str) {
-        // Write your code here
-        string rev=str;
-        reverse(rev.begin(),rev.end());
-        int size=str.size();
-        str+='$';
-        str+=rev;
-        int n=str.size();
-        vector<int>lps(n,0);
-        int pre=0, suf=1;
-        while(suf<str.size())
-        {
-            if(str[pre]==str[suf])
-            {
-                lps[suf]=pre+1;
-                pre++,suf++;
+    int minChar(string& s) {
+        // Step 1: Reverse the string
+        string reversed = s;
+        reverse(reversed.begin(), reversed.end());
+        
+        // Step 2: Create the combined string
+        string combined = s + '#' + reversed;
+        
+        // Step 3: Calculate LPS array
+        int n = combined.length();
+        vector<int> lps(n, 0); // LPS array initialized with 0s
+        
+        // Building the LPS array using the combined string
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];  // j stores the length of the previous matched prefix-suffix
+            while (j > 0 && combined[i] != combined[j]) {
+                j = lps[j - 1]; // We keep moving the pointer to the next possible match
             }
-            else{
-                if(pre==0)
-                    
-                    suf++;
-               else{
-                   pre=lps[pre-1];
-               }
+            if (combined[i] == combined[j]) {
+                j++; // If characters match, increment the length of matched prefix-suffix
             }
-            
-            }
-            
-            return size-lps[n-1];
+            lps[i] = j;  // Store the current length of matched prefix-suffix in LPS array
+        }
+        
+        // Step 4: Calculate minimum characters to add
+        // We need to add (s.length() - LPS[last index]) characters
+        return s.length() - lps.back();  // lps.back() gives us the length of the matched prefix-suffix
     }
 };
+
+
 
 
 //{ Driver Code Starts.
