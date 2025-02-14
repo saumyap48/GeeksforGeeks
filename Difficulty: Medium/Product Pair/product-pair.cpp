14ef12b64c4ce36e53d97ff78cafdd9d3c3b033c
Saumya Pandey <saumyap271@gmail.com>
2025-02-14 08:43:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Product Pair/product-pair.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    bool isProduct(vector<int> arr, long long x) {
+          int n=arr.size();
+        int start=0,end=n-1;
+        sort(arr.begin(),arr.end());
+        while(start<end){
+            long long product = 1LL * arr[start] * arr[end];
+            if(product==x)
+            return true;
+            else if(product<x)
+            start++;
+            else
+            end--;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        long long x;
+        cin >> x;
+        cin.ignore(); // To discard any leftover newline characters
+
+        Solution ob;
+        auto ans = ob.isProduct(arr, x);
+        cout << (ans ? "true\n" : "false\n");
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
