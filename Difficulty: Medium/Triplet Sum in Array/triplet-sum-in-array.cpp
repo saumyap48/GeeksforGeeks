9734f60c2f1086b8d0fe92a83b8a31e4ec478bc3
Saumya Pandey <saumyap271@gmail.com>
2025-02-15 19:33:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
@@ -6,49 +6,52 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-
-    // Should return true if there exists a triplet in the
-    // array arr[] which sums to x. Otherwise false
-    bool find3Numbers(int arr[], int n, int x) {
-        // Your Code Here
-          sort(arr,arr+n);
-        int sum=0;
-        
-        
-      for(int i=0;i<n-2;i++){
-          int j=i+1;
-          int k=n-1;
-      while(j<k){
-          sum=arr[i]+arr[j]+arr[k];
-          if(sum == x){
-              return true;
-          }
-          else if(sum > x){
-              k--;
-          }
-          else{
-              j++;
-          }
-    }
-      }
-      return false;
+    bool hasTripletSum(vector<int> &arr, int target) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=0; i<n-2; i++){
+            int answer=target-arr[i];
+            int start=i+1,end=n-1;
+            while(start<end){
+                if(arr[start]+arr[end]==answer)
+                return 1;
+                else if(arr[start]+arr[end]>answer)
+                end--;
+                else
+                start++;
+            }
+        }
+        return 0;
     }
 };
 
 //{ Driver Code Starts.
 
 int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int n, X;
-        cin >> n >> X;
-        int i, A[n];
-        for (i = 0; i < n; i++)
-            cin >> A[i];
-        Solution ob;
-        cout << ob.find3Numbers(A, n, X) << endl;
-    }
-}
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
 
+    while (t--) {
+        vector<int> arr;
+        int target;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> target;
+        cin.ignore(); // Ignore newline character after target input
+
+        Solution solution;
+        bool result = solution.hasTripletSum(arr, target);
+        cout << (result ? "true" : "false") << "\n";
+    }
+
+    return 0;
+}
 // } Driver Code Ends
