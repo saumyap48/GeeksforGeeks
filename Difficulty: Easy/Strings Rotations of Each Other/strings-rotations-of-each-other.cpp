8b28a40f8a7536a18e6fac73afe266630a9988ee
Saumya Pandey <saumyap271@gmail.com>
2025-02-21 12:12:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
+++ Difficulty: Easy/Strings Rotations of Each Other/strings-rotations-of-each-other.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
     // Function to check if two strings are rotations of each other or not.
@@ -12,7 +13,7 @@ class Solution {
             int n =s1.length();
               string t=s1+s1;
              if(n!=s2.length()) return 0;
-            return (t).find(s2)!=string::npos;
+          return(t).find(s2)!=string::npos;
     }
 };
 
