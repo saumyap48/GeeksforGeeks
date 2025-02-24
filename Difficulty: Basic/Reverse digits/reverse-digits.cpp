8b1b69d516bdfadba6b92d97a047fb44fa8f9ecc
Saumya Pandey <saumyap271@gmail.com>
2025-02-24 21:35:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse digits/reverse-digits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int reverseDigits(int n) {
+        int temp=0;
+        int answer=0;
+        while(n>0){
+            temp=n%10;
+            answer=10*answer+temp;
+            n=n/10;
+        }
+        return answer;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        int ans = ob.reverseDigits(n);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
