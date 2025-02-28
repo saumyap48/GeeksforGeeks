1d35a4aabe2d0eaf29af989d069223e87c475e6d
Saumya Pandey <saumyap271@gmail.com>
2025-02-28 15:35:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -5,14 +5,27 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution {
   public:
-    int gcd(int a, int b) {
-        if(b==0)
-            return a;
-            return gcd(b,a%b);
+    int gcd(int n1, int n2) {
+        while(n1 > 0 && n2 > 0) {
+
+            if(n1 > n2) {
+                n1 = n1 % n2;
+            }
+            
+            else {
+                n2 = n2 % n1;
+            }
+        }
+        if(n1 == 0) return n2;
+    
+        return n1;
+
     }
 };
+
 
 
 //{ Driver Code Starts.
