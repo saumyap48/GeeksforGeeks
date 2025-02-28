72cacefb9f85efee682834a13c709e976c29c699
Saumya Pandey <saumyap271@gmail.com>
2025-02-28 11:12:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Isomorphic Strings/isomorphic-strings.cpp
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.cpp
@@ -1,58 +1,54 @@
 //{ Driver Code Starts
 // C++ program to check if two strings are isomorphic
 
-
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 #define MAX_CHARS 256
 
+
 // } Driver Code Ends
-class Solution
-{
-    public:
-    //Function to check if two strings are isomorphic.
-    bool areIsomorphic(string s1, string s2)
-    {
-        int n=s1.size();
-        int m=s2.size();
-        
-        vector<int>v1(128,-1);
-        vector<int>v2(128,-1);
-        
-        if(n!=m){
-            return false;
-        }
-        
+
+class Solution {
+  public:
+    // Function to check if two strings are isomorphic.
+    bool areIsomorphic(string &s, string &t) {
+        int m1[256]={0},m2[256]={0};
+        int n=s.size();
         for(int i=0; i<n; i++){
-            if(v1[s1[i]]!=v2[s2[i]]){
-                return false;
-            }
-            v1[s1[i]]=i;
-            v2[s2[i]]=i;
+            if(m1[s[i]]!=m2[t[i]])
+            return false;
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
         }
         return true;
-        
-        
+ 
     }
 };
+
 
 //{ Driver Code Starts.
 
 // Driver program
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    string s1,s2;
+    cin >> t;
+    string s1, s2;
     while (t--) {
-        cin>>s1;
-        cin>>s2;
+        cin >> s1;
+        cin >> s2;
         Solution obj;
-        cout<<obj.areIsomorphic(s1,s2)<<endl;
+        int r = obj.areIsomorphic(s1, s2);
+        if (r) {
+            cout << "true" << endl;
+        } else {
+            cout << "false" << endl;
+        }
+
+        cout << "~"
+             << "\n";
     }
-    
+
     return 0;
 }
 // } Driver Code Ends
