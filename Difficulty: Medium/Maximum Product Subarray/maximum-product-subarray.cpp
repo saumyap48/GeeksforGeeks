5fadec614639d481ea537de2ef3a84470522302a
Saumya Pandey <saumyap271@gmail.com>
2025-03-23 20:52:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -5,23 +5,28 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function Template for C++
 class Solution {
   public:
     // Function to find maximum product subarray
-    int maxProduct(vector<int> &arr) {
-        long long maxProd=arr[0], minprod=arr[0], ans=arr[0];
-        for(int i=1; i<arr.size(); i++){
-            if(arr[i]<0){
-                swap(maxProd,minprod);
-            }
-            maxProd=max((long long)arr[i],maxProd*arr[i]);
-            minprod=min((long long)arr[i],minprod*arr[i]);
-            ans=max(ans,maxProd);
+    int maxProduct(vector<int> &nums) {
+        // Your Code Here
+        int n=nums.size();
+        int answer=INT_MIN;
+        int prefix=1,suffix=1;
+        for(int i=0; i<n; i++){
+            if(prefix==0)prefix=1;
+            if(suffix==0)suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[n-i-1];
+            answer=max(answer,max(prefix,suffix));
         }
-        return ans;
+      return answer;
     }
 };
+
+
 
 //{ Driver Code Starts.
 
