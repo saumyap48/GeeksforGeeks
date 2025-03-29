d50ec944090dd7af697638e878025372033f6a79
Saumya Pandey <saumyap271@gmail.com>
2025-03-29 12:27:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Upper Bound/implement-upper-bound.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int upperBound(vector<int>& nums, int target) {
+        // code here
+        int n=nums.size();
+        int low=0,high=n-1;
+        int answer=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>target){
+            answer=mid;
+            high=mid-1;
+            }
+            else
+            low=mid+1;
+        }
+        return answer;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.upperBound(arr, d);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
