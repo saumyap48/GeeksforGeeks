f6b10ccfb7bf042c74c920b2b59dfab0ad7bcaf1
Saumya Pandey <saumyap271@gmail.com>
2025-04-21 15:40:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.cpp
@@ -25,20 +25,40 @@ class Node {
 
 // } Driver Code Ends
 
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
 class Solution {
   public:
     Node* constructLL(vector<int>& arr) {
-        if(arr.empty()) return NULL;
-        
-        int n = arr.size();
-        Node* head = new Node(arr[0]);
-        Node* temp = head;
-        for(int i = 1; i < n; i++) {
-            temp->next = new Node(arr[i]);  
-            temp = temp->next;              
+        // code here
+        Node* head=new Node(arr[0]);
+        Node* mover=head;
+        for(int i=1; i<arr.size(); i++){
+            Node* temp=new Node(arr[i]);
+            mover->next=temp;
+            mover=temp;
         }
-        
-        return head;  
+        return head;
     }
 };
 
@@ -64,7 +84,7 @@ int main() {
             cout << ans->data << " ";
             ans = ans->next;
         }
-        cout << "\n";
+        cout << "\n~\n";
     }
     return 0;
 }
