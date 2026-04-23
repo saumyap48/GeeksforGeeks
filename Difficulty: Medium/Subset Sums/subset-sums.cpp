26f0d16efe3d59f180897df601b43e0d3b974a35
Saumya Pandey <saumyap271@gmail.com>
2026-04-23 17:36:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sums/subset-sums.cpp
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -1,48 +1,25 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-  public:
-  
-   void print(vector<int>&arr,int index,int n,int sum,vector<int>&ans){
-       if(index==n){
-           ans.push_back(sum);
-           return;
-       }
-       print(arr,index+1,n,sum,ans);
-       print(arr,index+1,n,sum+arr[index],ans);
-   }
-    vector<int> subsetSums(vector<int>arr, int n) {
-        vector<int>ans;
-        print(arr,0,arr.size(),0,ans);
+public:
+    void func(int ind, int sum, vector<int>& arr, int N, vector<int>& ans) {
+        if(ind == N) {
+            ans.push_back(sum);
+            return;
+        }
+        
+        // pick
+        func(ind + 1, sum + arr[ind], arr, N, ans);
+        
+        // not pick
+        func(ind + 1, sum, arr, N, ans);
+    }
+
+    vector<int> subsetSums(vector<int>& arr) {
+        int N = arr.size();
+        vector<int> ans;
+        
+        func(0, 0, arr, N, ans);
+        
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N;
-        cin >> N;
-        vector<int> arr(N);
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
-        Solution ob;
-        vector<int> ans = ob.subsetSums(arr, N);
-        sort(ans.begin(), ans.end());
-        for (auto sum : ans) {
-            cout << sum << " ";
-        }
-        cout << endl;
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-// } Driver Code Ends
